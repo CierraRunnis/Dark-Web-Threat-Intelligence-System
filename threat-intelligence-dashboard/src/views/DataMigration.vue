a292074c44a26748d8edc2c5ea829621f5b68422
CierraRunnis <CierraRunnis@users.noreply.github.com>
2026-08-27 09:46:17 +0800

    chore: prepare v20260827 release

--- threat-intelligence-dashboard/src/views/DataMigration.vue
+++ threat-intelligence-dashboard/src/views/DataMigration.vue
@@ -22,7 +22,7 @@
           <strong>数据迁移</strong>
         </div>
         <div class="header-actions">
-          <span class="app-version">版本 <strong>v20260826.1.0</strong></span>
+          <span class="app-version">版本 <strong>v20260827</strong></span>
           <button class="avatar" type="button" aria-label="个人账户"></button>
         </div>
       </header>
